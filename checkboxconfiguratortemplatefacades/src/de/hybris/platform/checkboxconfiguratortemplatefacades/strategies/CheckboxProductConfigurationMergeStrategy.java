e0c4b2c5b8844ae000281815078d7167a4b379dc
Eric.Li <libin539@tasly.com>
2017-01-12 16:31:41 +0800

    2.Add Implementation-Facades.

---
+++ checkboxconfiguratortemplatefacades/src/de/hybris/platform/checkboxconfiguratortemplatefacades/strategies/CheckboxProductConfigurationMergeStrategy.java
@@ -0,0 +1,40 @@
+package de.hybris.platform.checkboxconfiguratortemplatefacades.strategies;
+
+import de.hybris.platform.commercefacades.order.data.ConfigurationInfoData;
+import de.hybris.platform.commercefacades.product.strategies.merge.ProductConfigurationMergeStrategy;
+
+import javax.annotation.Nonnull;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+public class CheckboxProductConfigurationMergeStrategy implements ProductConfigurationMergeStrategy
+{
+	public static final String CHECKBOX_CHECKED_VALUE = "on";
+
+	@Nonnull
+    @Override
+    public List<ConfigurationInfoData> merge(@Nonnull final List<ConfigurationInfoData> baseConfiguration,
+                                             @Nonnull final List<ConfigurationInfoData> mergeConfiguration)
+    {
+        final List<String> formLabels = baseConfiguration.stream()
+                .peek(this::setConfigurationValueToBooleanString)
+                .map(ConfigurationInfoData::getConfigurationLabel)
+                .collect(Collectors.toList());
+
+        final List<ConfigurationInfoData> checkboxConfiguration = mergeConfiguration.stream()
+                .filter(checkBoxConfiguration -> !formLabels.contains(checkBoxConfiguration.getConfigurationLabel()))
+                .collect(Collectors.toList());
+
+        checkboxConfiguration.stream()
+                .forEach(configuration -> configuration.setConfigurationValue(Boolean.FALSE.toString()));
+
+        return checkboxConfiguration;
+    }
+
+	protected void setConfigurationValueToBooleanString(final ConfigurationInfoData configurationInfoData)
+	{
+		configurationInfoData.setConfigurationValue(Boolean.valueOf(
+				CHECKBOX_CHECKED_VALUE.equals(configurationInfoData.getConfigurationValue())).toString());
+	}
+}
