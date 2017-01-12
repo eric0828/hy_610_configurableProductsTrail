83a6dad7c376b2c68b6f0453f5fbb7fdeeb0a9cf
Eric.Li <libin539@tasly.com>
2017-01-12 14:16:58 +0800

    Configurable Products Trail:initialize(such as checkboxconfiguratortemplateservices,checkboxconfiguratortemplatefacades and checkboxconfiguratortemplateaddon).

---
+++ checkboxconfiguratortemplatefacades/src/de/hybris/platform/checkboxconfiguratortemplatefacades/setup/CheckboxconfiguratortemplatefacadesSystemSetup.java
@@ -0,0 +1,44 @@
+/*
+ * [y] hybris Platform
+ * 
+ * Copyright (c) 2000-2016 SAP SE
+ * All rights reserved.
+ * 
+ * This software is the confidential and proprietary information of SAP 
+ * Hybris ("Confidential Information"). You shall not disclose such 
+ * Confidential Information and shall use it only in accordance with the 
+ * terms of the license agreement you entered into with SAP Hybris.
+ */
+package de.hybris.platform.checkboxconfiguratortemplatefacades.setup;
+
+import static de.hybris.platform.checkboxconfiguratortemplatefacades.constants.CheckboxconfiguratortemplatefacadesConstants.PLATFORM_LOGO_CODE;
+
+import de.hybris.platform.core.initialization.SystemSetup;
+
+import java.io.InputStream;
+
+import de.hybris.platform.checkboxconfiguratortemplatefacades.constants.CheckboxconfiguratortemplatefacadesConstants;
+import de.hybris.platform.checkboxconfiguratortemplatefacades.service.CheckboxconfiguratortemplatefacadesService;
+
+
+@SystemSetup(extension = CheckboxconfiguratortemplatefacadesConstants.EXTENSIONNAME)
+public class CheckboxconfiguratortemplatefacadesSystemSetup
+{
+	private final CheckboxconfiguratortemplatefacadesService checkboxconfiguratortemplatefacadesService;
+
+	public CheckboxconfiguratortemplatefacadesSystemSetup(final CheckboxconfiguratortemplatefacadesService checkboxconfiguratortemplatefacadesService)
+	{
+		this.checkboxconfiguratortemplatefacadesService = checkboxconfiguratortemplatefacadesService;
+	}
+
+	@SystemSetup(process = SystemSetup.Process.INIT, type = SystemSetup.Type.ESSENTIAL)
+	public void createEssentialData()
+	{
+		checkboxconfiguratortemplatefacadesService.createLogo(PLATFORM_LOGO_CODE);
+	}
+
+	private InputStream getImageStream()
+	{
+		return CheckboxconfiguratortemplatefacadesSystemSetup.class.getResourceAsStream("/checkboxconfiguratortemplatefacades/sap-hybris-platform.png");
+	}
+}
